3ed333dab883d4104591283f82d7bd2460303d11
Peterson <petersonpaganelli@outlook.com>
2023-02-27 17:37:39 -0300

    Requisito 1

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,36 @@
+import { STRING, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+  },
+  teamName: {
+    type: STRING(30),
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Teams;
