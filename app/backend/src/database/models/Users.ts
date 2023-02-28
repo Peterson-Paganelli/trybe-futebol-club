8e104e8aff63ee1fd52c58b9bde1d422f9a2d43b
Peterson <petersonpaganelli@outlook.com>
2023-02-28 11:35:45 -0300

    feat: create database Users

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,40 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+
+  role: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
