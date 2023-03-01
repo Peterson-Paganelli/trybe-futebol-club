58023c4aed78130a5f9656c2ef0dfcafe78a13bd
Peterson <petersonpaganelli@outlook.com>
2023-03-01 12:23:00 -0300

    Requisito 10

--- app/backend/src/controllers/middlewares/loginValidation.ts
+++ app/backend/src/controllers/middlewares/loginValidation.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+import Joi = require('joi');
 
 const validateLogin = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  const validateEmailFunc = Joi.string().email().required();
+  const validateEmail = validateEmailFunc.validate(email);
+  if (validateEmail.error || password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   next();
 };
--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -6,12 +6,15 @@ import Users from '../database/models/Users';
 class UserService {
   public postLogin = async (payload: Login) => {
     const { email } = payload;
+
     const user = await Users.findOne({ where: { email } });
-    if (!user || !(await compare(payload.password, user.password))) {
-      return { status: 401, response: { message: 'Incorrect email or password' } };
+    if (!user) return { status: 401, response: { message: 'Invalid email or password' } };
+    const password = await compare(payload.password, user.password);
+    if (password) {
+      const token = generateToken({ user });
+      return { status: 200, response: { token } };
     }
-    const token = generateToken({ user });
-    return { status: 200, response: { token } };
+    return { status: 401, response: { message: 'Invalid email or password' } };
   };
 }
 export default UserService;
